856184e279e851420b8decc8729158b6ddf1e641
lovachittumuri <lova.anand@gmail.com>
2022-08-17 23:16:06 +0530

    @Lova Chittumuri - First Commit - Spring - Boot - applications

---
+++ learn-spring-boot/src/test/java/com/lova/kss/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lova.kss.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
