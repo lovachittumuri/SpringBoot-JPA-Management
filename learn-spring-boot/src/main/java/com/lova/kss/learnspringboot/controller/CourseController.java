eefbf71d02d463ef27b1e8e72f1744566e46c20f
lovachittumuri <lova.anand@gmail.com>
2022-08-18 23:32:45 +0530

    @Lova Chittumuri - Adding the additional Features - Actuator and its features.

--- learn-spring-boot/src/main/java/com/lova/kss/learnspringboot/controller/CourseController.java
+++ learn-spring-boot/src/main/java/com/lova/kss/learnspringboot/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lova.kss.learnspringboot.bean.Course;
@@ -13,6 +14,13 @@ public class CourseController {
 
 	@GetMapping("/courses")
     public List<Course> getAllCourses() {
-		return Arrays.asList(new Course(1, "Java 8 ", "Lova Chittumuri"));
+		return Arrays.asList(new Course(1, "Java 8 ", "Lova Chittumuri"),new Course(2, "Spring Microservices ", "Karuna Chittumuri"));
 	}
+	
+	@GetMapping("/courses/{id}")
+    public Course getAllCourses(@PathVariable long id) {
+		return (new Course(2, "Spring Microservices ", "Karuna Chittumuri"));
+	}
+	
+	
 }
